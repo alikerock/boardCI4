6080630d0daf5cc071ea304c8516b865add9ec58
rock's ezweb <alikerock@gmail.com>
2023-09-04 11:45:50 +0900

    sql update

--- boardci4.sql
+++ boardci4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 23-09-04 02:36
+-- 생성 시간: 23-09-04 04:44
 -- 서버 버전: 10.4.28-MariaDB
 -- PHP 버전: 8.2.4
 
@@ -40,7 +40,11 @@ CREATE TABLE `board` (
 --
 
 INSERT INTO `board` (`bid`, `userid`, `subject`, `content`, `regdate`) VALUES
-(1, 'hong', '제목 테스트', '내용 테스트', '2023-09-01 05:12:33');
+(1, 'hong', '제목 테스트', '내용 테스트', '2023-09-01 05:12:33'),
+(2, 'test', '입력 테스트', '내용 테스트', '0000-00-00 00:00:00'),
+(3, 'test', '입력 테스트2', '내용 테스트2', '2023-09-04 11:37:03'),
+(4, 'test', '입력 테스트3', 'test3', '2023-09-04 11:39:03'),
+(5, 'test', '입력 테스트4', 'test4', '2023-09-04 11:43:43');
 
 --
 -- 덤프된 테이블의 인덱스
@@ -60,7 +64,7 @@ ALTER TABLE `board`
 -- 테이블의 AUTO_INCREMENT `board`
 --
 ALTER TABLE `board`
-  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
