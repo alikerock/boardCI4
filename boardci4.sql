2799ef68e30c9e7b01893d2beaee28748713f8a7
rock's ezweb <alikerock@gmail.com>
2023-09-04 09:37:14 +0900

    sql update

---
+++ boardci4.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 23-09-04 02:36
+-- 서버 버전: 10.4.28-MariaDB
+-- PHP 버전: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `boardci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `board`
+--
+
+CREATE TABLE `board` (
+  `bid` int(10) NOT NULL,
+  `userid` varchar(50) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `regdate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `board`
+--
+
+INSERT INTO `board` (`bid`, `userid`, `subject`, `content`, `regdate`) VALUES
+(1, 'hong', '제목 테스트', '내용 테스트', '2023-09-01 05:12:33');
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`bid`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `board`
+--
+ALTER TABLE `board`
+  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
