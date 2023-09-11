0387422a4075c279f8662928a58c49d2d23298c4
rock's ezweb <alikerock@gmail.com>
2023-09-11 12:22:56 +0900

    modify add

--- app/Views/board_write.php
+++ app/Views/board_write.php
@@ -8,11 +8,11 @@
     </div>
     <div class="mb-3">
       <label for="subject" class="form-label">제목</label>
-      <input type="text" class="form-control" id="subject" name="subject" placeholder="제목을 입력하세요">
+      <input type="text" class="form-control" id="subject" name="subject" placeholder="제목을 입력하세요" value="<?php echo isset($view->subject)? $view->subject:''; ?>">
     </div>
     <div class="mb-3">
       <label for="content" class="form-label">내용</label>
-      <textarea class="form-control" id="content" name="content" rows="3" ></textarea>
+      <textarea class="form-control" id="content" name="content" rows="3" ><?php echo isset($view->content)? $view->content:''; ?></textarea>
     </div>
     <button class="btn btn-primary">등록</button>
 </form>
